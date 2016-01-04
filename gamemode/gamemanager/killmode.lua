5360cb63c9bd039856286ea63275dc71bd7156fe
PraeluceantBoreus <richard.stoeckl@aon.at>
2016-01-04 11:51:20 +0100

    replaced killmode

--- gamemode/gamemanager/killmode.lua
+++ gamemode/gamemanager/killmode.lua
@@ -1,4 +1,4 @@
--- uses SteamID64
+-- uses GetName
 Killmode = {}
 local TS_Hunter_Victim = {}
 local TS_Identities = {}
@@ -18,14 +18,14 @@ local function isHunter(st_h, st_v)
 end
 
 local function name(ply)
-    return TS_Identities[ply:SteamID64()]
+    return TS_Identities[ply:GetName()]
 end
 
 local function newVictim(hunter, victim)
-    TS_Hunter_Victim[hunter:SteamID64()] = victim:SteamID64()
+    TS_Hunter_Victim[hunter:GetName()] = victim:GetName()
     Killmode.roundState(hunter, R_STATE.HUNTING)
     net.Start("Victim")
-        net.WriteString(TS_Identities[victim:SteamID64()])
+        net.WriteString(TS_Identities[victim:GetName()])
     net.Send(hunter)
 end
 
@@ -46,7 +46,7 @@ end
 
 
 local function giveBackName(ply)
-    local oldname = TS_Identities[ply:SteamID64()]
+    local oldname = TS_Identities[ply:GetName()]
     if oldname ~= nil then
         table.insert(TEMP_NAMES, oldname)
         TEMP_NAMES = Arrays.shuffle(TEMP_NAMES)
@@ -63,12 +63,12 @@ end
 local function newIdentity(ply)
 --     create second table for names, remove name, when in use, add name, when nobody has it, shuffle names
     giveBackName(ply)
-    TS_Identities[ply:SteamID64()] = table.remove(TEMP_NAMES, 1)
+    TS_Identities[ply:GetName()] = table.remove(TEMP_NAMES, 1)
     
     net.Start("Identity")
         net.WriteUInt(0,1)
-        net.WriteString(ply:SteamID64())
-        net.WriteString(TS_Identities[ply:SteamID64()])
+        net.WriteString(ply:GetName())
+        net.WriteString(TS_Identities[ply:GetName()])
     net.Broadcast()
 end
 
@@ -81,14 +81,14 @@ end
 local function roundState(ply, state, all)
     if all ~= nil then
         for nr,val in pairs(player.GetAll()) do
-            TS_Round_State[val:SteamID64()] = state
+            TS_Round_State[val:GetName()] = state
         end
         net.Start("RoundState")
         net.WriteUInt(1,1)
         net.WriteTable(TS_Round_State)
         net.Broadcast()
     else
-        local st_id = ply:SteamID64()
+        local st_id = ply:GetName()
         TS_Round_State[st_id] = state
         net.Start("RoundState")
         --is table?
@@ -116,14 +116,14 @@ end
 
 hook.Add("PlayerDisconnected", "GiveBackName", function(ply)
     giveBackName(ply)
-    table.remove(TS_Identities, ply:SteamID64())
+    table.remove(TS_Identities, ply:GetName())
     for hunt,vic in pairs(TS_Hunter_Victim) do
-        if vic == ply:SteamID64() then
+        if vic == ply:GetName() then
             newVictim(hunt,byName(vic))
         end
     end
-    table.remove(TS_Hunter_Victim, ply:SteamID64())
-    table.remove(TS_Round_State, ply:SteamID64())
+    table.remove(TS_Hunter_Victim, ply:GetName())
+    table.remove(TS_Round_State, ply:GetName())
 end)
 
 
